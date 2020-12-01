43f9c7c0051710dc894ba06d69c9b461e128b41a
leowang <leowang@jetfuel.com.tw>
2020-12-01 17:25:50 +0800

    add server

---
+++ src/xhr.ts
@@ -0,0 +1,9 @@
+import { AxiosRequestConfig } from './types'
+
+export default function xhr(config: AxiosRequestConfig) {
+  let { data = null, method = 'get', url } = config
+
+  const request = new XMLHttpRequest()
+  request.open(method.toUpperCase(), url)
+  request.send()
+}
