de7d7229a2abeafe0c91b42a7da35279eaa15153
leowang <leowang@jetfuel.com.tw>
2020-12-25 09:28:14 +0800

    ✨ add type AxiosReqeustConfig baseURL

--- src/types/index.ts
+++ src/types/index.ts
@@ -29,6 +29,7 @@ export interface AxiosRequestConfig {
   xsrfCookieName?: string
   xsrfHeaderName?: string
   auth?: AxiosBasicCredentials
+  baseURL?: string
   paramsSerializer?: (param: any) => string
   validateStatus?: (status: number) => boolean
   onDownloadProgress?: (e: ProgressEvent) => void
