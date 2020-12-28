2bfe82918f1e5dccad8f5be76476f4c98e31bf8a
leowang <leowang@jetfuel.com.tw>
2020-12-28 14:45:28 +0800

    ✨ add helpers/data, error, headers unit test

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -8,6 +8,14 @@ export function isObject(val: any): val is Object {
   return toString.call(val) === '[object Object]'
 }
 
+export function isFormData(val: any): boolean {
+  return typeof val !== 'undefined' && val instanceof FormData
+}
+
+export function isURLSearchParams(val: any): val is URLSearchParams {
+  return typeof val !== 'undefined' && val instanceof URLSearchParams
+}
+
 export function extend<T, U>(to: T, from: U): T & U {
   for (const key in from) {
     ;(to as T & U)[key] = from[key] as any
@@ -34,12 +42,4 @@ export function deepMerge(...objs: any[]): any {
     }
   })
   return result
-}
-
-export function isFormData(val: any): boolean {
-  return typeof val !== 'undefined' && val instanceof FormData
-}
-
-export function isURLSearchParams(val: any): val is URLSearchParams {
-  return typeof val !== 'undefined' && val instanceof URLSearchParams
 }
